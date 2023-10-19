1f73fb68c48a03d839d6fbe0c6e763e61c3b235a
ankitaSsi <104835257+ankitaSsi@users.noreply.github.com>
2023-10-19 19:51:49 +0530

    program

---
+++ comment.py
@@ -0,0 +1,17 @@
+
+"""this is a single line comment
+print("Single line Comment")
+print("Welcome to ssi")"""
+
+print("Multiline comment")
+
+
+#docstring comment
+
+def demo():
+    '''This is docstring comment'''
+    print("Executed Function")
+
+
+demo()#function call
+print(demo.__doc__)
---
+++ typeconver.py
@@ -0,0 +1,6 @@
+n1=12
+n2=int(12.33)
+res=n1+n2
+print("n1 :",type(n1))
+print("n2 :",type(n2))
+print("res:",type(res))
